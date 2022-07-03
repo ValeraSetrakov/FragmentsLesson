63a73047a90b8cfd301240079ad3bd7d8e663466
valerijsetrakov <valerasetrakov@gmail.com>
2022-07-03 18:22:59 +0300

    Исправлена назначение фабрики фрагментов

--- app/src/main/java/com/valerasetrakov/fragmentslesson/MainActivity.kt
+++ app/src/main/java/com/valerasetrakov/fragmentslesson/MainActivity.kt
@@ -15,10 +15,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportFragmentManager.fragmentFactory =
+            CustomFragmentFactory(FilterCommunicator())
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportFragmentManager.fragmentFactory = CustomFragmentFactory(FilterCommunicator())
         if (savedInstanceState == null) {
             showChats()
             supportFragmentManager.setFragmentResultListener(
